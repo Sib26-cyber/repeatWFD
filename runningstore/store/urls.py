9312af098f7a5395599552e74459a165587b276b
Sibeil <sibeil@yahoo.ie>
2025-08-14 14:26:58 +0100

    changes to registry and virt

---
+++ runningstore/store/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   
+    path('', views.home, name='home'),  # Home page
+    path('about/', views.about, name='about'),  # About page
+    path('login/', views.login_user, name='login'),  # Login page
+    path('logout/', views.logout_user, name='logout'),  # Logout page
+    path('register/', views.register_user, name='register'),  # Registration page
+]
