3856d19287244f38374be810d3e5f0aeb5fdb132
Sibeil <sibeil@yahoo.ie>
2025-08-15 22:57:56 +0100

    form changes

--- runningstore/store/forms.py
+++ runningstore/store/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm,SetPasswordForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, SetPasswordForm
 from django import forms
 
 
