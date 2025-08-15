76e4d6cf8612e97146c07b8f352ce9dc9571dfba
Sibeil <sibeil@yahoo.ie>
2025-08-15 17:25:22 +0100

    categories

--- runningstore/store/urls.py
+++ runningstore/store/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),  # Login page
     path('logout/', views.logout_user, name='logout'),  # Logout page
     path('register/', views.register_user, name='register'),  # Registration page
-    path('product/<int:pk>/', views.product, name='product'),  # Product page)    
+    path('product/<int:pk>/', views.product, name='product'),  # Product page
+    path('category/<str:foo>/', views.category, name='category')  # Category page
 ]
