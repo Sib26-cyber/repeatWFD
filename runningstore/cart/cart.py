d781d225f686c817bfa62e4179f732b0512d4ad1
Sibeil <sibeil@yahoo.ie>
2025-08-19 23:13:30 +0100

    Adding product to cart

--- runningstore/cart/cart.py
+++ runningstore/cart/cart.py
@@ -1,13 +1,22 @@
 
 class Cart():
     def __init__(self, request):
-    self.session = request.session
+        self.session = request.session
 
 #Get the current session key if it exists
-    cart = self.session.get('session_key')
+        cart = self.session.get('session_key')
     #if the user is new, no session key! Create one
-    if 'session_key' not in request.session:
-        cart = self.session['session_key']={}
+        if 'session_key' not in request.session:
+            cart = self.session['session_key']={}
 
     #Make sure cart is available on all pages of the site
-    self.cart = cart
+        self.cart = cart
+    def add(self, product):
+        product_id = str(product_id)
+
+        #Logic
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+        self.session.modified = True
