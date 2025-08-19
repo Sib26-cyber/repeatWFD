1d72c9fed8ae59d0c0fe0189748808e7a6c884b9
Sibeil <sibeil@yahoo.ie>
2025-08-19 12:56:24 +0100

    cart

--- runningstore/urls.py
+++ runningstore/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls')),  # Include URLs from the store app
-    path('cart/', include ('cart.urls')),
+    path('', include('store.urls')),  # Include URLs from the store app   
+    
+
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Serve media files in development
