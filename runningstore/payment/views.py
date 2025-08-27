921e042be3c8fb9ffd8ceaf46d079defe0c97f81
Sibeil <sibeil@yahoo.ie>
2025-08-28 00:16:40 +0100

    Order processing working with generation of order items

--- runningstore/payment/views.py
+++ runningstore/payment/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from payment.models import ShippingAddress,Order, OrderItem
 from payment.forms import ShippingForm, PaymentForm
 from django.contrib.auth.models import User
+from store.models import Product
 
 
 
@@ -60,37 +61,83 @@ def checkout(request):
         return render(request, "payment/checkout.html",{"cart_products":cart_products, "quantities":quantities, "totals":totals })
                    
 def process_order(request):
-    if request.POST:
-        #Get cart details
-        cart = Cart(request)
-        cart_products = cart.get_prods
-        quantities = cart.get_quants
-        totals = cart.cart_total()
+         if request.POST:
+            #Get cart details
+            cart = Cart(request)
+            cart_products = cart.get_prods
+            quantities = cart.get_quants
+            totals = cart.cart_total()
 
-        payment_form = PaymentForm(request.POST or None)
-        #Get shipping session data
-        my_shipping = request.session.get('my_shipping')
-        #Create Shipping Address from session data
-        full_name = my_shipping['shipping_full_name']
-        email = my_shipping['shipping_email']
-        amount_paid = totals
+            payment_form = PaymentForm(request.POST or None)
+            #Get shipping session data
+            my_shipping = request.session.get('my_shipping')
+            #Create Shipping Address from session data
+            full_name = my_shipping['shipping_full_name']
+            email = my_shipping['shipping_email']
+            amount_paid = totals
 
-        shipping_address = f"{my_shipping['shipping_address_line1']}\n {my_shipping['shipping_address_line2']}\n {my_shipping['shipping_city']}\n {my_shipping['shipping_state']}\n {my_shipping['shipping_postal_code']}\n{my_shipping['shipping_country']}"
-        #Get cart details
-        user = request.user if request.user.is_authenticated else None
-        create_order = Order.objects.create(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
-        create_order.save()
+            shipping_address = f"{my_shipping['shipping_address_line1']}\n {my_shipping['shipping_address_line2']}\n {my_shipping['shipping_city']}\n {my_shipping['shipping_state']}\n {my_shipping['shipping_postal_code']}\n{my_shipping['shipping_country']}"
+            #Get cart details
+            user = request.user if request.user.is_authenticated else None
+            create_order = Order.objects.create(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+            
+            #Create Order Items
+            order_id = create_order.pk
+            quants = quantities() if callable(quantities) else quantities
+            for product in cart_products():
+                #get product id 
+                product_id = product.id
+                #get product price
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+                    
+                for key, value in quants.items():
+                    if int(key) == product.id:                               
+            
+            
+            
+            
+            
+                        Create_order_item = OrderItem.objects.create(order_id=order_id, product_id=product_id, user = user, quantity=value, price=price)
+                        Create_order_item.save()                                           
+                                      
+                
+            messages.success(request, "Order Processed Successfully!")
+            return redirect('home')     
+            
         
-        messages.success(request, "Order Processed Successfully!")
-        return redirect('home')       
+         else:             
+            #not logged in 
+            create_order = Order.objects.create( full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+            messages.success(request, "not logged in ")
+            return redirect('home')            
+                        
+                        #create order item
+                   
+            
+                
+                
+            
+            
         
-    else:
-        #not logged in 
-        create_order = Order.objects.create( full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
-        create_order.save()
-        messages.success(request, "not logged in ")
-        return redirect('home')
-        else: 
+        
+        
+        
+        
+        
+        
+            
+        
+            
+             
+                           
+                                                 
+            
+
 
         
         
